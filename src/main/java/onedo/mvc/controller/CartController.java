2b3f3eca8c097bd680ee70f20555f4908089da05
soobin <a01056239778@gmail.com>
2022-04-16 11:10:30 +0900

    cart.jsp와 cartController연결

--- src/main/java/onedo/mvc/controller/CartController.java
+++ src/main/java/onedo/mvc/controller/CartController.java
@@ -1,6 +1,7 @@
 package onedo.mvc.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -17,105 +18,143 @@ import onedo.mvc.service.CartServiceImpl;
 import onedo.mvc.service.GoodsService;
 import onedo.mvc.service.GoodsServiceImpl;
 
-
 //@WebServlet("/cart")
-public class CartController implements Controller{
-	
+public class CartController implements Controller {
+
 	private GoodsService goodsService = new GoodsServiceImpl();
 	private CartService cartService = new CartServiceImpl();
 
 	private Map<String, CartDTO> cartMap = new HashMap<>();
 
 	/**
-	 * 장바구니에 물건 담기 
+	 * 장바구니에 물건 담기
+	 * 
 	 * @param request
 	 * @param response
 	 * @return
 	 * @throws Exception
 	 */
 	public ModelAndView insert(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		String userId = request.getParameter("userId");
 		String goodsCode = request.getParameter("goodsCode");
 		GoodsDTO goods = goodsService.selectByGoodsCode(goodsCode, false);
 		int amount = Integer.parseInt(request.getParameter("amount"));
 
 		CartDTO cart = null;
-		
-		if(cartMap.containsKey(userId)) {
+
+		if (cartMap.containsKey(userId)) {
 			cart = cartMap.get(userId);
-		}else {
+		} else {
 			cart = new CartDTO(userId);
 			cartMap.put(userId, cart);
 		}
-		
+
 		cartService.insert(cart, goods, amount);
-		
-		
-		return new ModelAndView("cart.html", true); // 원래의 장바구니넣기한 상세페이지 머물러있어야 함!!
+
+		return new ModelAndView("cart.jsp", true); // 원래의 장바구니넣기한 상세페이지 머물러있어야 함!!
 	}
-	
-	
+
 	/**
-	 * 장바구니 조회 
+	 * 장바구니 조회
+	 * 
 	 * @param request
 	 * @param response
 	 * @return
 	 * @throws Exception
 	 */
 	public ModelAndView select(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		
+
 		String userId = request.getParameter("userId");
-		
+
 		CartDTO cart = cartMap.get(userId);
-		
-		if(cart==null) {
+
+		if (cart == null) {
 			// " 장바구니가 비어있습니다"
-		}else {
+			request.setAttribute("cartItemList", new ArrayList<>());
+		} else {
 			List<CartItemDTO> cartItemList = cart.getCartItemList();
-			request.setAttribute("cartItemList", cartItemList); //model에 데이터를 담아 보내는 역할
-			
+			request.setAttribute("cartItemList", cartItemList); // model에 데이터를 담아 보내는 역할
+
 		}
-		
-		return new ModelAndView("cart.html");
+
+		checkTotalPrice(request);
+
+		return new ModelAndView("cart.jsp");
 	}
 
-	public ModelAndView increaseAmount(HttpServletRequest request, HttpServletResponse response)throws Exception{
-		
+	/**
+	 * 결제금액 및 배송비 체크
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	public void checkTotalPrice(HttpServletRequest request) throws Exception {
+
+		String userId = request.getParameter("userId");
+		if (!cartMap.containsKey(userId)) {
+			request.setAttribute("totalItemPrice", 0);
+			request.setAttribute("deliveryPrice", 0);
+			request.setAttribute("paymentPrice", 0);
+			return;
+		}
+		int totalItemPrice = 0;
+		int sumTotalItemPrice = 0;
+		int deliveryPrice = 3000;
+		List<CartItemDTO> cartItemList = cartMap.get(userId).getCartItemList();
+		for (CartItemDTO cartItem : cartItemList) {
+			totalItemPrice = cartItem.getGoods().getGoodsPrice() * cartItem.getAmount();
+			sumTotalItemPrice += totalItemPrice;
+			if (sumTotalItemPrice > 15000) {
+				deliveryPrice = 0;
+			}
+		}
+		request.setAttribute("totalItemPrice", sumTotalItemPrice);
+		request.setAttribute("deliveryPrice", deliveryPrice);
+		request.setAttribute("paymentPrice", (sumTotalItemPrice - deliveryPrice));
+	}
+
+	public ModelAndView increaseAmount(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		String userId = request.getParameter("userId");
 		String goodsCode = request.getParameter("goodsCode");
 
 		List<CartItemDTO> cartItemList = cartMap.get(userId).getCartItemList();
-		
-		for(CartItemDTO cartItem : cartItemList) {
-			if(cartItem.getGoods().getGoodsCode()== Integer.parseInt(goodsCode)) {
+
+		for (CartItemDTO cartItem : cartItemList) {
+			if (cartItem.getGoods().getGoodsCode() == Integer.parseInt(goodsCode)) {
 				int originAmount = cartItem.getAmount();
 				cartItem.setAmount(++originAmount);
 			}
-			
-		}
-		return new ModelAndView("cart.html");
-	}
-	
 
-	public ModelAndView decreaseAmount(HttpServletRequest request, HttpServletResponse response)throws Exception{
-		
+		}
+		request.setAttribute("cartItemList", cartItemList);
+		return new ModelAndView("cart.jsp");
+	}
+
+	public ModelAndView decreaseAmount(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
 		String userId = request.getParameter("userId");
 		String goodsCode = request.getParameter("goodsCode");
 
 		List<CartItemDTO> cartItemList = cartMap.get(userId).getCartItemList();
-		
-		for(CartItemDTO cartItem : cartItemList) {
-			if(cartItem.getGoods().getGoodsCode()== Integer.parseInt(goodsCode)) {
+
+		for (CartItemDTO cartItem : cartItemList) {
+			if (cartItem.getGoods().getGoodsCode() == Integer.parseInt(goodsCode)) {
 				int originAmount = cartItem.getAmount();
-				cartItem.setAmount(--originAmount);
+				if(originAmount>0) {
+					cartItem.setAmount(--originAmount);
+				}else {
+					cartItem.setAmount(0);
+				}
 			}
-			
+
 		}
-		return new ModelAndView("cart.html");
+		request.setAttribute("cartItemList", cartItemList);
+		return new ModelAndView("cart.jsp");
 	}
-	
-	
+
 	@Override
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
