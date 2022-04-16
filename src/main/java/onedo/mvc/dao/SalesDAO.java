bf7deb04bcc73d9eba8bf9f5cf9c1b2df44db4a7
soobin <a01056239778@gmail.com>
2022-04-17 03:45:09 +0900

    매출 통계 관련 DTO, DAO 추가

---
+++ src/main/java/onedo/mvc/dao/SalesDAO.java
@@ -0,0 +1,13 @@
+package onedo.mvc.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import onedo.mvc.dto.SalesDTO;
+
+public interface SalesDAO {
+
+	List<SalesDTO> selectAll() throws SQLException;
+	List<SalesDTO> selectGroupByGoodsCode() throws SQLException;
+	
+}
