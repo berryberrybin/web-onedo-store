a49f52a19aff0ceb8c9afa23570ba5f47227f780
soobin <a01056239778@gmail.com>
2022-04-17 03:46:01 +0900

    매출관리 중 상품별 매출조회 코드 추가

---
+++ src/main/java/onedo/mvc/service/SalesService.java
@@ -0,0 +1,16 @@
+package onedo.mvc.service;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import onedo.mvc.dto.SalesDTO;
+
+
+
+public interface SalesService {
+	
+	List<SalesDTO> selectAll() throws SQLException;
+
+	List<SalesDTO> selectGroupByGoodsCode() throws SQLException;
+	
+}
